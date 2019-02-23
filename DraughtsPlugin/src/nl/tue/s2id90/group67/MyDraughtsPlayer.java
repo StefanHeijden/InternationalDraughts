12e16397f9b4eb4c931d1785da45c2e923a1d78f
stefan <stefanheijden1@gmail.com>
2019-02-23 14:09:00 +0100

    Onthoudt nu beste keuze voor elke zet en bouwt daarop uit, is nu ook mogelijk om depth van +100 te krijgen. Toevoegen dat meer moves onthouden worden

--- DraughtsPlugin/src/nl/tue/s2id90/group67/MyDraughtsPlayer.java
+++ DraughtsPlugin/src/nl/tue/s2id90/group67/MyDraughtsPlayer.java
@@ -4,6 +4,7 @@ import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Integer.MIN_VALUE;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
 import nl.tue.s2id90.group67.AIStoppedException;
@@ -19,6 +20,10 @@ import java.lang.Math.*;
 public class MyDraughtsPlayer  extends DraughtsPlayer{
     private int bestValue=0;
     int maxSearchDepth;
+    Move[][] listOfPreviousMoves;
+    int[] alpha;
+    int[] beta;
+    int currentDepth;
     
     /** boolean that indicates that the GUI asked the player to stop thinking. */
     private boolean stopped;
@@ -31,28 +36,62 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
     @Override public Move getMove(DraughtsState s) {
         Move bestMove = null;
         bestValue = 0;
+        maxSearchDepth = 100;
         DraughtsNode node = new DraughtsNode(s);    // the root of the search tree
-        try {
+        // Get the current state of the board
+        DraughtsState state = node.getState();
+
+        // Get the current moves for the state
+        List<Move> firstMoves = state.getMoves();
+        Collections.shuffle(firstMoves);
+        // put a if statement for if no moves are found????
+        
+        // This list is used to remember the all the bestmoves for each move
+        listOfPreviousMoves = new Move[firstMoves.size()][];
+
+        // Add the current moves to the listOfPreviousMoves
+        for(int i=0;i<firstMoves.size();i++){
+            listOfPreviousMoves[i] = new Move[maxSearchDepth + 1];
+            listOfPreviousMoves[i][0] = firstMoves.get(i);
+        }
+        alpha = new int[maxSearchDepth + 1];
+        beta = new int[maxSearchDepth + 1];
+        
+        try {           
             // Iterative Deepening on AlphaBeta 
-            int depth = 1;
-            
-            while(true) { 
-                // compute bestMove and bestValue in a call to alphabeta
-                bestValue = alphaBeta(node, MIN_VALUE, MAX_VALUE, depth);
+            currentDepth = 1;
+            while(currentDepth <= maxSearchDepth) {
+                int index = 0;
+                //List<List<Move>> copyOfListOfPreviousMoves = listOfPreviousMoves;
+                for(Move[] moveList : listOfPreviousMoves) {
+                    DraughtsNode newNode = node;
+                    DraughtsState newState;
+                    for(Move m : moveList) {
+                        if(m == null) {
+                            break;
+                        }
+                        // Set the state of the board to after doing the next move in the list
+                        newState = newNode.getState();
+                        newState.doMove(m);
+                        newNode = new DraughtsNode(newState);
+                    }
+                    newState = newNode.getState();
+                    
+                     // compute bestMove and bestValue in a call to alphabeta
+                    alphaBeta(newState, index);
+                    index++;
+                }
+                currentDepth++;
                 
-                // store the bestMove found uptill now
-                // NB this is not done in case of an AIStoppedException in alphaBeat()
-                bestMove  = node.getBestMove();
-                
-                // print the results for debugging reasons
-                System.err.format(
-                "%s: depth= %2d, best move = %5s, value=%d\n", 
-                this.getClass().getSimpleName(),depth, bestMove, bestValue
-                );
-                // Increase depth
-                depth++;
+                // for testing only, not neccessary in real run
+                bestMove = calcBestMove(state);
             }
-        } catch (AIStoppedException ex) {  /* nothing to do */  }
+        } catch (AIStoppedException ex) {
+            
+            System.out.println("Stopped in getMove()");
+        }
+    
+        bestMove = calcBestMove(state);
         
         if (bestMove==null) {
             System.err.println("no valid move found!");
@@ -61,6 +100,36 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
             return bestMove;
         }
     } 
+    
+    Move calcBestMove(DraughtsState state) {
+        int indexForBestMove = 0;
+        if (state.isWhiteToMove()) {
+            bestValue = alpha[0];
+            for(int i = 1;i<listOfPreviousMoves.length;i++) {
+                if(alpha[i] < bestValue) {
+                    bestValue = alpha[i];
+                    indexForBestMove = i;
+                }
+            }
+        } else  {
+            bestValue = beta[0];
+            for(int i = 1;i<listOfPreviousMoves.length;i++) {
+                if(beta[i] > bestValue) {
+                    bestValue = beta[i];
+                    indexForBestMove = i;
+                }
+            }
+        }
+        
+        // print the results for debugging reasons
+        System.err.format(
+        "%s: depth= %2d, best move = %5s, value=%d\n", 
+        this.getClass().getSimpleName(),currentDepth - 1, 
+        listOfPreviousMoves[indexForBestMove][0], bestValue
+        );
+        
+        return listOfPreviousMoves[indexForBestMove][0];
+    }
 
     /** This method's return value is displayed in the AICompetition GUI.
      * 
@@ -93,13 +162,13 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
      * @return the computed value of this node
      * @throws AIStoppedException
      **/
-    int alphaBeta(DraughtsNode node, int alpha, int beta, int depth)
+    void alphaBeta(DraughtsState state, int index)
             throws AIStoppedException
     {
-        if (node.getState().isWhiteToMove()) {
-            return alphaBetaMax(node, alpha, beta, depth);
+        if (state.isWhiteToMove()) {
+            alphaBetaMax(state, index);
         } else  {
-            return alphaBetaMin(node, alpha, beta, depth);
+             alphaBetaMin(state, index);
         }
     }
     
@@ -120,7 +189,7 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
      * @return the compute value of this node
      * @throws AIStoppedException thrown whenever the boolean stopped has been set to true.
      */
-     int alphaBetaMax(DraughtsNode node, int alpha, int beta, int depth)
+     void alphaBetaMax(DraughtsState state, int index)
             throws AIStoppedException {
         // Stop the search if a certain amount of time has passed
         if (stopped) { 
@@ -129,88 +198,90 @@ public class MyDraughtsPlayer  extends DraughtsPlayer{
             throw new AIStoppedException(); 
         }
         
-        // Get the current state of the board
-        DraughtsState state = node.getState();
-        
-        // Get the current moves for the state
-        List<Move> newMoves = state.getMoves();
+        // Retreive the available moves for this state
+        List<Move> moves = state.getMoves();
         
         // If no moves are possible return the evaluation of the current state
-        if (depth == 0 || newMoves.isEmpty()) {
-            return evaluate(state);
-        }
-        
-        // Evaluate each possible move for the state of the board
-        Move currentBestMove = newMoves.get(0);
-        while (!newMoves.isEmpty()) { // Continue untill no new moves are found
-            
-            // Set the state of the board to after doing the next move in the list
-            state.doMove(newMoves.get(0));
-            DraughtsNode newNode = new DraughtsNode(state);
-            
-            // Get the best move of the opponent
-            int newAlpha = alphaBetaMin(newNode, alpha, beta, depth - 1);
-            
-            // Set the state of the board to before the next move
-            state.undoMove(newMoves.get(0));
-            
-            // Check whether the next move is better then the previous ones
-            if (newAlpha > alpha) {
-                currentBestMove = newMoves.get(0);
-                alpha = newAlpha;
+        if (moves.isEmpty()) {// Heb je hier niet gewoon verloren of gewonnen?
+            System.out.println("No moves found");
+        }else{
+            // Evaluate each possible move for the state of the board
+            Move currentBestMove = moves.get(0);
+            for (Move m : moves) { // Continue untill no new moves are found
+                
+                // Evaluate the the result of the next move
+                int result = evaluateMove(state, m);
+
+                // Check whether the next move is better then the previous ones
+                if (result > alpha[index]) {
+                    currentBestMove = m;
+                    alpha[index] = result;
+                }
+                //.....
+                if (alpha[index] >= beta[index]) {
+                    currentBestMove = m;
+                    break;
+                    // alpha[index] = beta[index];
+                }
             }
-            //.....
-            if (alpha >= beta) {
-                node.setBestMove(newMoves.get(0));
-                return beta;
-            }
-            // Remove the next move in the list
-            newMoves.remove(0);
+            // Save the best move
+            listOfPreviousMoves[index][currentDepth] = currentBestMove;
         }
-        
-        // Save the best move and alpha of that move
-        node.setBestMove(currentBestMove);
-        return alpha;
      }
-     
-    
-    int alphaBetaMin(DraughtsNode node, int alpha, int beta, int depth)
+
+    void alphaBetaMin(DraughtsState state, int index)
             throws AIStoppedException {
         if (stopped) { 
             stopped = false; 
             System.out.println("stopped in AlphaBetaMin");
             throw new AIStoppedException(); 
         }
+
+        // Retreive the available moves for this state
+        List<Move> moves = state.getMoves();
         
-        DraughtsState state = node.getState();
-        //String test = "max " + depth + ": ";
-        List<Move> newMoves = state.getMoves();
-        if (depth == 0 || newMoves.isEmpty()) {
-            return evaluate(state);
-        }
-        Move currentBestMove = newMoves.get(0);
-        while (!newMoves.isEmpty()) {
-            state.doMove(newMoves.get(0));
-            DraughtsNode newNode = new DraughtsNode(state);
-            int newBeta = alphaBetaMax(newNode, alpha, beta, depth - 1);
-            //test += newBeta + " ";
-            state.undoMove(newMoves.get(0));
-            if (newBeta < beta) {
-                currentBestMove = newMoves.get(0);
-                beta = newBeta;
+        // If no moves are possible return the evaluation of the current state
+        if (moves.isEmpty()) {// Heb je hier niet gewoon verloren of gewonnen?
+            System.out.println("No moves found");
+        }else{
+            // Evaluate each possible move for the state of the board
+            Move currentBestMove = moves.get(0);
+            for (Move m : moves) { // Continue untill no new moves are found
+                
+                // Evaluate the the result of the next move
+                int result = evaluateMove(state, m);
+
+                // Check whether the next move is better then the previous ones
+                if (result < beta[index]) {
+                    currentBestMove = m;
+                    beta[index] = result;
+                }
+                //.....
+                if (beta[index] <= alpha[index]) {
+                    currentBestMove = m;
+                    break;
+                    // alpha[index] = beta[index];
+                }
             }
-            if (beta <= alpha) {
-                node.setBestMove(newMoves.get(0));
-                //System.out.println(test);
-                return alpha;
-            }
-            newMoves.remove(0);
+            // Save the best move
+            listOfPreviousMoves[index][currentDepth] = currentBestMove;
         }
-        
-        node.setBestMove(currentBestMove);
-        //System.out.println(test);
-        return beta;
     }
+    
+      /** A method that evaluates the given state after a certain move. */
+      int evaluateMove(DraughtsState state, Move m) {
+        // Set the state of the board to after doing the next move in the list
+        state.doMove(m);
+        DraughtsNode newNode = new DraughtsNode(state);
+
+        // Evaluate the current state
+        int result = evaluate(newNode.getState());
+
+        // Set the state of the board to before the next move
+        state.undoMove(m);
+        
+        return result;
+      }
 
     /** A method that evaluates the given state. */
     // ToDo: write an appropriate evaluation function
