064a12b4c53db1b046f81b4581f351021f0edf2b
stefan <stefanheijden1@gmail.com>
2019-02-26 12:55:13 +0100

    adde some coments in tt and fixed some changes that were lost

--- DraughtsPlugin/src/nl/tue/s2id90/group67/TranspositionTable.java
+++ DraughtsPlugin/src/nl/tue/s2id90/group67/TranspositionTable.java
@@ -10,23 +10,45 @@ import org10x10.dam.game.Move;
  * @author stefa
  */
 public class TranspositionTable {
+    // Current bit string for the state of the board
+    // Used as index for the list of Simple States
     private int hash;
+    
+    // All the different bitstrings for each possible 
     private int[][] bitStrings;
+    
+    // Bitstring of whether it is whites turn
+    // Xoring this with the hash switches the turn for the same board
     private int isWhiteToMove;
+    
+    // The list with all of the states previously searched with AlphaBeta
     private SimpleState[] states = new SimpleState [Integer.MAX_VALUE];
     
     TranspositionTable() {
+        // The checker board has 5 different types of pieces: white piece, white king
+        // white king, black piece, black king, empty and has 50 different positions
+        // where it can be
         bitStrings = new int[5][50];
+        
+        // Determine a unique bitstring for each combination of piece and position
+        // with zobric hashing
         for (int i = 0; i <5; i++) {
             for (int j = 0; j < 50; j++) {
                 bitStrings[i][j] = (int) (((long) (Math.random() * Long.MAX_VALUE)) & 0xFFFFFFFF);
             }
         }
-        hash = 0;
+        
+        // Determine a unique bitstring for whether it is white turn
+        // Used to switch turns
         isWhiteToMove = (int) (((long) (Math.random() * Long.MAX_VALUE)) & 0xFFFFFFFF);
+        
+        // Set hash to empty board
+        hash = 0;
     }
     
+    // Initializes the hash to a certain board state
     public int setBoard(int[] board, boolean whiteTurn) {
+        hash = 0;
         for(int i = 1 ; i<board.length;i++) {
              hash = hash ^ bitStrings[board[i]][i];
         }
@@ -36,21 +58,29 @@ public class TranspositionTable {
         return hash;
     }
     
-    public SimpleState getInformation(){
+    // Returns the information of the current state
+    public SimpleState retrieve(){
         if(states[hash] != null) {
             return states[hash];
         }
         return null;
     }
     
+    public void store(Move bestMove, int bestScore, int type, int depthSearched) {
+        states[hash] = new SimpleState(bestMove, bestScore, type, depthSearched);
+    }
+    
+    // Returns the hash(index) for the current state
     public int getBoard(){
         return hash;
     }
     
+    // Update the the current board by doing a certain move
     public void doMove(Move move) {
         
     }
     
+    // Update the the current board by undoing a certain move
     public void undoMove(Move move) {
         
     }
